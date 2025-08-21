6ebd7e1424badd7f5c3281e4bf4e927a58542df5
emiciai <emilia.chan700@education.nsw.gov.au>
2025-08-21 15:05:23 +1000

    work on wheel

---
+++ project/lib/test/wheeltest.py
@@ -0,0 +1,6 @@
+from wheel import Wheel
+from time import sleep
+
+pinL = 20
+pinR = 18
+
--- project/lib/wheel.py
+++ project/lib/wheel.py
@@ -2,46 +2,50 @@ import time
 from servo import Servo
 from machine import Pin, PWM
 
-class Wheel:
-    def __init__(self, pinleft, pinright, debug=False):
-        self.__debug = debug
-        self.pinL = PWM(Pin(20))
-        self.pinR = PWM(Pin(18))
-        self.pinL.freq(50)
-        self.pinR.freq(50)
 
-    def left(self):
-        if self.__debug:
-            self.pinR.set_duty(1300)
-            self.pinL.set_duty(1500)
-            time.sleep(2.85)
+servo_pwm = PWM(Pin(18))
+servo_pwm2 = PWM(Pin(20))
 
-    def right(self):
-        if self.__debug:
-            self.pinR.set_duty(1500)
-            self.pinL.set_duty(1700)
-            time.sleep(2.25)
 
-    def forward(self):
-        if self.__debug:
-            self.pinR.set_duty(500)
-            self.pinL.set_duty(2500)
-            time.sleep(2)
+freq = 50
+min_us = 500
+max_us = 2500
+dead_zone_us = 1500
 
-    def backward(self):
-        if self.__debug:
-            self.pinR.set_duty(2500)
-            self.pinL.set_duty(500)
-            time.sleep(2)
+my_servo = Servo(
+    pwm=servo_pwm, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq
+)
 
-    def pause(self):
-        if self.__debug:
-            self.pinR.set_duty(1500)
-            self.pinL.set_duty(1500)
-            time.sleep(2)
+my_servo2 = Servo(
+    pwm=servo_pwm2, min_us=min_us, max_us=max_us, dead_zone_us=dead_zone_us, freq=freq
+)
 
-    def stop(self):
-        if self.__debug:
-            self.pinR.stop()
-            self.pinL.stop()
-            time.sleep(2)
+def forward():
+    my_servo.set_duty(500)
+    my_servo2.set_duty(2500)
+    time.sleep(2)
+
+def left():
+    my_servo.set_duty(1300)
+    my_servo2.set_duty(1500)
+    time.sleep(2.85)
+
+def right():
+    my_servo.set_duty(1500)
+    my_servo2.set_duty(1700)
+    time.sleep(2.25)
+
+def backward():
+    my_servo.set_duty(2500)
+    my_servo2.set_duty(500)
+    time.sleep(2)
+
+def pause():
+    my_servo.set_duty(1500)
+    my_servo2.set_duty(1500)
+    time.sleep(2)
+
+def stop():
+    my_servo.stop()
+    my_servo2.stop()
+    time.sleep(2)
